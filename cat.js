154c46a94a93112fdcd98499f7ba3baf83c40707
MDN <mark.denardo@gmail.com>
2022-08-31 02:21:32 -0400

    Create cat.js

---
+++ cat.js
@@ -0,0 +1,10 @@
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//🐈‍⬛
+//MDN
+//IG: markdenardo
+const f=(()=>a.fft[0])
+const arr=[]
+for(i=0;i<100;i++){arr.push(i)}
+bpm=900
+shape(arr.fit(8).fast(100)).rotate(-5,-5).modulate(o0,0.5).modulateScrollY(noise(1,1,1)).out(o0)
+render(o0)
