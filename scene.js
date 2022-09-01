2ff1854da4a2912737ef7e49dd0659171e19d294
MDN <mark.denardo@gmail.com>
2022-08-31 22:44:54 -0400

    Create scene.js

---
+++ scene.js
@@ -0,0 +1,7 @@
+const arr=[1,2,3]
+const arr1=(arr.map(n=>1+n))
+console.log('arr1',arr1)
+const f=(()=>a.fft[2])
+voronoi(1,1,1).color(f).modulate(o0,Math.PI).out(o0)
+shape(arr1).modulatePixelate(osc(1,1,1)).modulate(o1,0.5).add(o0).out(o1)
+render(o1)
