f770512f836bf7c05a2197ac626e77a5af6f0475
MDN <mark.denardo@gmail.com>
2022-10-05 21:39:18 -0400

    Update new-moon.js

--- new-moon.js
+++ new-moon.js
@@ -1,6 +1,12 @@
 // new moon
 // mdn
-const f = ()=>a.fft[0]
-voronoi(1,1,1).rotate(1,1).color(10,1,100).modulate(o1,Math.PI).scale(0.5,f).out(o0)
+
+
+voronoi(1,1,1).color(10).modulate(o1,Math.PI).out(o0)
 src(o0).out(o1)
 render(o0)
+
+//const f = ()=>a.fft[0]
+//voronoi(1,1,1).rotate(1,1).color(10,1,100).modulate(o1,Math.PI).scale(0.5,f).out(o0)
+//src(o0).out(o1)
+//render(o0)
