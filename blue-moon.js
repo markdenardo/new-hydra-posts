cf0e382c917c2085fa00b6958d3a11eed7dfbe1e
MDN <mark.denardo@gmail.com>
2022-10-05 21:28:34 -0400

    Update and rename blue-moon.js to new-moon.js

--- blue-moon.js
+++
@@ -1,3 +0,0 @@
-voronoi(1,1,1).color(10).modulate(o1,Math.PI).out(o0)
-src(o0).out(o1)
-render(o0)
---
+++ new-moon.js
@@ -0,0 +1,6 @@
+// new moon
+// mdn
+const f = ()=>a.fft[0]
+voronoi(1,1,1).rotate(1,1).color(10,1,100).modulate(o1,Math.PI).scale(0.5,f).out(o0)
+src(o0).out(o1)
+render(o0)
